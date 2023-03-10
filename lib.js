aa9e3c3fc97f5c280de9edb6ecf2bae9ff9aebdf
szr <songzhouran@qq.com>
2023-03-10 17:48:28 +0800

    close by comments

--- lib.js
+++ lib.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-globals */
-import { Octokit } from '@octokit/rest';
+const { Octokit } = require('@octokit/rest');
 
 const doCloseIssue = async function (token, repo, issue_number) {
   const octokit = new Octokit({
@@ -21,17 +21,14 @@ const doCloseIssue = async function (token, repo, issue_number) {
   }
 };
 
-export async function closeIssue(argv) {
+exports.closeIssue = async function (argv) {
   const maxPerPage = 100;
   const octokit = new Octokit({
     auth: argv.token,
   });
   const commits = await octokit.request(
-    `GET /repos/kungfu-trader/${argv.repo}/pulls/${argv.pullRequestNumber}/commits`,
+    `GET /repos/kungfu-trader/${argv.repo}/issues/${argv.pullRequestNumber}/comments`,
     {
-      owner: 'kungfu-trader',
-      repo: argv.repo,
-      pull_number: argv.pullRequestNumber,
       per_page: maxPerPage,
       headers: {
         'X-GitHub-Api-Version': '2022-11-28',
@@ -40,7 +37,7 @@ export async function closeIssue(argv) {
   );
   const re = /#\d+/g;
   for (const element of commits.data) {
-    const issues = element.commit.message.match(re);
+    const issues = element.body.match(re);
     if (issues) {
       console.log('issues', issues);
       for (const it of issues) {
@@ -48,4 +45,4 @@ export async function closeIssue(argv) {
       }
     }
   }
-}
+};
